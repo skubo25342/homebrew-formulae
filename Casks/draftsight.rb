b42e000f00722177eabcdac1bd9a0ed12869869a
kubo_shosei@kubo-mbpr13e2015 <s.kubo.25342@gmail.com>
2015-10-12 17:27:27 +0900

    draftsight 2015 SP3

---
+++ Casks/draftsight.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'draftsight' do
+  version '2015 SP3'
+  sha256 'a87063af1e984ddfbd7b5db46aebb23f708f7b77ad0e8fed77942a98d13fc94c'
+
+  url 'http://dl-ak.solidworks.com/nonsecure/draftsight/V1R6.3-2/DraftSight.dmg'
+  name 'DraftSight'
+  homepage 'http://www.3ds.com/ja/products-services/draftsight-cad-software/'
+  license :unknown
+
+  app 'DraftSight.app'
+end
