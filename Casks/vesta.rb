617c291eba64f163c15c5f43d4b9d6a58a18a9bf
kubo_shosei@kubo-mbpr13e2015 <s.kubo.25342@gmail.com>
2015-10-12 16:59:49 +0900

    vesta 3.3.1

---
+++ Casks/vesta.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'vesta' do
+  version '3.3.1'
+  sha256 '1f6ad173df3311e3bc914c6f909cac84c9aa0541ce9968616ac84435bbbfd658'
+
+  url 'http://www.geocities.jp/kmo_mma/crystal/download/VESTA.dmg'
+  name 'VESTA'
+  homepage 'http://jp-minerals.org/vesta/jp/'
+  license :unknown
+
+  suite 'VESTA'
+end
